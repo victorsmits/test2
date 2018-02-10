413458e0b7d0f228cb543d954e65a0f710463356
victor smits <16107@ecam.be>
2018-02-10 13:19:37 +0100

    test

--- test_utils.py
+++ test_utils.py
@@ -2,6 +2,7 @@
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
 
+
 import unittest
 from utils import *
 class TestUtils(unittest.TestCase):
@@ -23,4 +24,4 @@ if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
     runner = unittest.TextTestRunner()
     exit(not runner.run(suite).wasSuccessful())
-    
+#test
